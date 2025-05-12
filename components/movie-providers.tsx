985ee68256f8ea1cb06421192e73b9ead53747d2
leedbswo107 <ljyu1@naver.com>
2025-05-12 15:13:33 +0900

    remove: test output

--- components/movie-providers.tsx
+++ components/movie-providers.tsx
@@ -3,7 +3,6 @@ import { API_URL } from "../app/constants";
 const getProviders = async (id: string) => {
   const res = await fetch(`${API_URL}/${id}/providers`);
   const json = await res.json();
-  console.log(">>>>>>", json.US);
   return json;
 };
 
